d7a5290e7ba10a033575b2ce75a4ec48b299e0f0
sagnik-bhattacharya <sagnikbhattacharya1506@gmail.com>
2024-09-08 14:03:39 +0530

    Updated Project base

--- android/app/src/main/kotlin/com/example/deliveristo_flutter_challenge/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/deliveristo_flutter_challenge/MainActivity.kt
@@ -2,5 +2,4 @@ package com.codingliquids.deliveristo_flutter_challenge
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
